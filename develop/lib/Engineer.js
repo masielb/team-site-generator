965713fac0abf4c2db8894d4c870599ecf5bf642
Masiel Bautista <masiel5@users.noreply.github.com>
2020-08-30 16:50:08 -0400

    engineer class

--- develop/lib/Engineer.js
+++ develop/lib/Engineer.js
@@ -1,1 +1,17 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+
+const Employee = require("./Employee");
+
+class Engineer extends Employee {
+    constructor(name, id, email, githubUserId) {
+        super(name, id, email);
+        this.role = "Engineer";
+        this.github = githubUserId;
+    }
+  
+    getGithub() {
+      return this.github;
+    }
+  }
+  
+  module.exports = Engineer;
